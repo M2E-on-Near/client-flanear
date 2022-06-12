c34d1c64939f4373341e324da5f49b44e8f3f9a7
Umbrien <kalugin.serej@gmail.com>
2022-06-12 04:51:49 +0300

    icons

--- util/Header.js
+++ util/Header.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context"
 import Ionicons from "react-native-vector-icons/Ionicons";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
@@ -11,17 +11,23 @@ export default function Header() {
         <SafeAreaView style={{ backgroundColor: "#F3F0DF" }}>
             <View style={styles.header}>
                 <View style={[styles.box, { borderBottomRightRadius: 20, borderTopRightRadius: 20 }]}>
-                    <Ionicons name={"wallet"} size={30} />
+                    <Image
+                        source={require("../assets/KRKicon.png")}
+                        style={{width: 30, height: 30, resizeMode: "stretch"}}
+                    />
                     <Text style={[styles.InterSemiBold, { fontSize: 20 }]}>{krkBalance}</Text>
                 </View>
                 <View style={[styles.box, { borderBottomLeftRadius: 20, borderTopLeftRadius: 20 }]}>
                     <Text style={[styles.InterSemiBold, { fontSize: 20 }]}>3</Text>
-                    <FontAwesome5 name={"coins"} size={30} />
+                    <Image
+                        source={require("../assets/fla-near-icon.png")}
+                        style={{width: 30, height: 30, resizeMode: "stretch"}}
+                    />
                 </View>
             </View>
             <WebView source={{uri: 'http://192.168.0.141:3000'}} style={{display: 'none'}}
                      onMessage={(event) => {
-                         alert(event.nativeEvent.data);
+                         // alert(event.nativeEvent.data);
                          setKrkBalance(event.nativeEvent.data);
                      }}
                      injectedJavaScriptBeforeContentLoaded={`
