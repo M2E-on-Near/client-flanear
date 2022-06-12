02a4cccb9a5b24c33871cef953f9a0e5fa3468a7
Umbrien <kalugin.serej@gmail.com>
2022-06-12 05:06:43 +0300

    url update

--- screens/Play.js
+++ screens/Play.js
@@ -6,7 +6,7 @@ export default function Play() {
     return (
         <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
             {/*TODO make it fullscreen on separate page*/}
-            <WebView source={{uri: 'https://m2e-on-near.github.io/dino'}} style={{height: 300, width: 400}} />
+            <WebView source={{uri: 'https://m2e-on-near.github.io/dino?img=https://raw.githubusercontent.com/M2E-on-Near/token-images/main/80.png'}} style={{height: 300, width: 400}} />
         </View>
     );
 }
